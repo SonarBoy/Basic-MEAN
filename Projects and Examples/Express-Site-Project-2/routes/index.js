623f9e32ffc9981af16ec195b99888fb10214e2a
SonarBoy <Joshua.L99.C@gmail.com>
2019-05-09 01:48:56 -0400

    Added the notes

--- Projects and Examples/Express-Site-Project-2/routes/index.js
+++ Projects and Examples/Express-Site-Project-2/routes/index.js
@@ -6,10 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*GET Services Page */
 router.get('/Services', function(req, res, next) {
   res.render('services', { title: 'Services' });
 });
 
+/*GET About Page */
 router.get('/About', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
