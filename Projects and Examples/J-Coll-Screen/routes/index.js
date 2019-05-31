5169e883723b34e90245414274de228b6ffee78d
SonarBoy <Joshua.L99.C@gmail.com>
2019-05-31 13:41:47 -0400

    Simple Create added

--- Projects and Examples/J-Coll-Screen/routes/index.js
+++ Projects and Examples/J-Coll-Screen/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var planet = require('../model/planet');
+var planetModel = require('../model/planet');
 
 /* GET home page. */
 router.get('/', function(request, response, next) {
@@ -22,7 +22,7 @@ router.get('/Services',function(request,response,next){
 });
 
 router.get('/Planets',(request,response,next) => {
-  planet.find((error,planetList) => {
+  planetModel.find((error,planetList) => {
 
     if(error){
       return console.error(error);
@@ -36,6 +36,34 @@ router.get('/Planets',(request,response,next) => {
             
     }
   });
+});
+
+//THIS IS ALL FOR POSTING INTO THE MONGO DATABASE
+router.get('/Planets-Add',(request,response,next) => {
+
+  response.render('planets/add',{
+    title: 'Add New Celestial Object'    
+  });
+
+});
+
+router.post('/Planets-Add',(request,response,next) => {
+
+  //THIS IS THE RETRIVAL OF THE DATA FROM THE ADD FORM TO 
+  //BE THEN PUT INTO THE DATABASE
+  let newObject  = planetModel({"Name": request.body.Name,
+  "Description":request.body.Description});
+
+  planetModel.create(newObject, (err,planetModel) => {
+    if(err){
+      console.log(err);
+      response.end(err);
+    }else{
+      //refresh to the planet list
+      response.redirect('/Planets');
+    }
+  });
+
 });
 
 module.exports = router;
