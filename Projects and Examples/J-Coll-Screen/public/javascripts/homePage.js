b5d5b94c3545ee2d6c35818f02f7266416bbafa4
SonarBoy <Joshua.L99.C@gmail.com>
2019-06-04 21:35:00 -0400

    Added Delete

--- Projects and Examples/J-Coll-Screen/public/javascripts/homePage.js
+++ Projects and Examples/J-Coll-Screen/public/javascripts/homePage.js
@@ -6,7 +6,12 @@
 
     function Start(){
         //alert("Hello ALl");
-
+        $('.btn-danger').click(function(event){
+            if(!confirm("Are you sure you want to delete ths item?")){
+                event.preventDefault();
+                window.location.assign("/Planets");
+            }
+        });
     }
 
     window.addEventListener("load",Start);
--- Projects and Examples/J-Coll-Screen/routes/index.js
+++ Projects and Examples/J-Coll-Screen/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('mongoose');
-var planetModel = require('../model/planet');
+var planetController = require('../controllers/planetController');
+
 
 /* GET home page. */
 router.get('/', function(request, response, next) {
@@ -21,92 +21,26 @@ router.get('/Services',function(request,response,next){
   response.render('services',{service: 'Service: ', alertStyle:'Danger'});
 });
 
-router.get('/Planets',(request,response,next) => {
-  planetModel.find((error,planetList) => {
 
-    if(error){
-      return console.error(error);
-    }else{
-            //Here we will later do a response.render
+router.get('/Planets',planetController.displayPlanetList);
 
-                response.render('planets/index',{
-                    title: 'Planet List',
-                    planetList: planetList
-                })
-            
-    }
-  });
-});
 
 //THIS IS ALL FOR POSTING INTO THE MONGO DATABASE
-router.get('/Planets-Add',(request,response,next) => {
-
-  response.render('planets/add',{
-    title: 'Add New Celestial Object'    
-  });
-
-});
-
+router.get('/Planets-Add',planetController.addPlanetsDisplay);
 
 //THIS IS THE RETRIVAL OF THE DATA FROM THE ADD FORM TO 
 //BE THEN PUT INTO THE DATABASE
-router.post('/Planets-Add',(request,response,next) => {
-
-  let newObject  = planetModel({"Name": request.body.Name,
-  "Description":request.body.Description});
-
-  planetModel.create(newObject, (err,planetModel) => {
-    if(err){
-      console.log(err);
-      response.end(err);
-    }else{
-      //refresh to the planet list
-      response.redirect('/Planets');
-    }
-  });
-
-});
+router.post('/Planets-Add',planetController.addPlanet);
 
 //GET REQUEST FOR THE EDIT PAGE NOTE THAT BOOTSTRAP DOES NOT WORK CURRENTLY ON 
 //THIS LEVEL OF ROUTE.
-router.get('/edit/:id',(request,response,next) => {
-  let id = request.params.id;
-
-  planetModel.findById(id, (error, planetObject) => {
-
-    if(error){
-      console.log(error);
-      response.end(error);
-    }else{
-      response.render('planets/edit',{
-        title: 'Edit Object',
-        planet: planetObject
-      });
-    }
-  });
-});
-
+router.get('/edit/:id',planetController.editPlanetGET);
 
 //POST REQUEST TO UPDATE THE DATA BASE FROM THE EDIT PAGE
-router.post('/edit/:id',(request,response,next) =>{
-  let id = request.params.id;
+router.post('/edit/:id',planetController.editPlanetPOST);
 
-  let updatedPlanet  = planetModel({
-    "_id" : id,
-    "Name": request.body.Name,
-    "Description" : request.body.Description
-  });
-
-  planetModel.update({_id:id},updatedPlanet,(error) =>{
-    if(error){
-      console.log(error);
-      response.end(error);
-    }else{
-      //refresh to the planet list
-      response.redirect('/Planets');
-    }
-  });
-});
+//ROUTER DELETE FUNCTIONALITY
+router.get('/delete/:id',planetController.deletePlanet);
 
 module.exports = router;
 
