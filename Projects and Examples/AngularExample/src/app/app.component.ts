4592d5853ba755f4557eb3f23b4d75734b8fbe93
SonarBoy <Joshua.L99.C@gmail.com>
2019-09-25 15:35:33 -0400

    Testing with angular

--- Projects and Examples/AngularExample/src/app/app.component.ts
+++ Projects and Examples/AngularExample/src/app/app.component.ts
@@ -1,10 +1,13 @@
+//Import of the decorator
 import { Component } from '@angular/core';
 
+//Parameterized componenet
 @Component({
+  //index.html look for app - root to install the whole app
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'This is a Testing Act';
+  title = 'THis is the start of working with angular';
 }
